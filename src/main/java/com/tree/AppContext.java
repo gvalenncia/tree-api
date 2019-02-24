57dbf9b7e3c6cd22a0de9bdfb313ad5777f48d75
Germán Valencia Vargas <gvalenncia@gmail.com>
2019-02-24 11:15:50 -0500

    Initial project structure

---
+++ src/main/java/com/tree/AppContext.java
@@ -0,0 +1,13 @@
+package com.tree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppContext {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppContext.class, args);
+	}
+
+}
