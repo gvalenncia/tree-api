71983c957f43d2dd8176cf00e20ab3457698c8a6
Germán Valencia Vargas <gvalenncia@gmail.com>
2019-02-24 16:35:36 -0500

    Implementing a function to traverse the tree in order

--- src/test/java/com/tree/service/domain/BinaryTreeTest.java
+++ src/test/java/com/tree/service/domain/BinaryTreeTest.java
@@ -1,10 +1,14 @@
 package com.tree.service.domain;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BinaryTreeTest {
@@ -17,6 +21,18 @@ public class BinaryTreeTest {
         MockitoAnnotations.initMocks(binaryTree);
     }
 
+    @Test
+    public void shouldTraverseInOrderTest(){
+        //given
+        this.binaryTree.addValue(84);
+        this.binaryTree.addValue(78);
+        this.binaryTree.addValue(49);
 
+        //when
+        List<Integer> outcome = this.binaryTree.traverseInOrder();
+
+        //then
+        Assert.assertArrayEquals(new Integer[]{49, 78, 84}, outcome.toArray());
+    }
 
 }
